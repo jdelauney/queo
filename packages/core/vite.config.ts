23c5844417443fe350d390a75d9ea2580426378f
Sebastienpanda <sebastienweb27@gmail.com>
2024-11-28 15:09:06 +0100

    feat: add composant button

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -7,36 +7,21 @@ export default defineConfig({
 		dts({
 			include: ['src/**/*.ts'],
 			outDir: 'dist/types',
-			cleanVueFileName: true,
 		}),
 	],
 	build: {
 		lib: {
 			entry: resolve(__dirname, 'src/index.ts'),
 			name: 'taylux',
-			fileName: (format) => `taylux.${format}.js`,
 		},
 		rollupOptions: {
-			external: ['react', 'react-dom'],
-			output: [
-				{
-					format: 'es',
-					exports: 'named',
-					assetFileNames: 'assets/[name][extname]',
-				},
-				{
-					format: 'umd',
-					name: 'taylux',
-					exports: 'named',
-					assetFileNames: 'assets/[name][extname]',
-				},
-			],
+			input: resolve(__dirname, 'src/styles/main.scss'),
+			output: {
+				assetFileNames: 'assets/[name][extname]',
+			},
 		},
 		sourcemap: false,
 		minify: true,
 		cssCodeSplit: false,
-		cssMinify: true,
-		reportCompressedSize: true,
-		chunkSizeWarningLimit: 1000,
 	},
 });
---
+++ packages/react/src/index.ts
@@ -0,0 +1,2 @@
+import '@taylux/core/src/styles/main.scss';
+export { Button } from './components/Button/button';
