b19bae8443184fbdf3bdcd32e216f57b865e1bd3
Sebastienpanda <sebastienweb27@gmail.com>
2024-11-29 10:40:25 +0100

    feat: testing composant Tag finish

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,41 +1,14 @@
-import { Button } from '@queo/react';
-import { Circle, Loader2, Trash, User } from 'lucide-react';
+import { Tag } from '@queo/react';
 
 function App() {
 	return (
 		<>
-			<Button size="sm">Hello world</Button>
-			<Button size="md">Hello world</Button>
-			<Button size="lg">Hello world</Button>
-
-			<Button variant="primary">Hello world</Button>
-			<Button variant="secondary">Hello world</Button>
-			<Button variant="outline">Hello world</Button>
-			<Button variant="ghost">Hello world</Button>
-			<Button variant="danger">Hello world</Button>
-
-			<Button icon={<User />}>User Settings</Button>
-			<Button icon={<User />} iconRight={<Trash />}>
-				User Settings
-			</Button>
-
-			<Button iconRight={<User />}>User Settings</Button>
-
-			<Button loading loadingIcon={<Loader2 className="animate-spin" />}>
-				User Settings
-			</Button>
-			<Button loading loadingPosition="left" loadingIcon={<Loader2 className="animate-spin" />}>
-				User Settings
-			</Button>
-			<Button loading loadingPosition="center" loadingIcon={<Loader2 className="animate-spin" />}></Button>
-			<Button disabled>User Settings</Button>
-			<Button loading loadingPosition="center" loadingIcon={<Circle className="animate-pulse" />}></Button>
-			<Button
-				loading
-				loadingPosition="center"
-				variant="outline"
-				loadingIcon={<Circle className="animate-bounce" />}
-			></Button>
+			<Tag variant="primary">Hello world</Tag>
+			<Tag variant="secondary">Hello world</Tag>
+			<Tag variant="outline">Hello world</Tag>
+			<Tag variant="ghost">Hello world</Tag>
+			<Tag variant="danger">Hello world</Tag>
+			<Tag isActive>Hello world</Tag>
 		</>
 	);
 }
--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -1,58 +1,13 @@
-<script setup lang="ts">
-import { Loader2, User } from 'lucide-vue-next';
-</script>
+<script setup lang="ts"></script>
 
 <template>
 	<div class="demo">
-		<Button size="sm">Hello world</Button>
-		<Button size="md">Hello world</Button>
-		<Button size="lg">Hello world</Button>
-
-		<Button variant="primary">Hello world</Button>
-		<Button variant="secondary">Hello world</Button>
-		<Button variant="outline">Hello world</Button>
-		<Button variant="ghost">Hello world</Button>
-		<Button variant="danger">Hello world</Button>
-
-		<Button>
-			<template #icon>
-				<User />
-			</template>
-			User Settings
-		</Button>
-		<Button>
-			User Settings
-			<template #iconRight>
-				<User />
-			</template>
-		</Button>
-		<Button>
-			<template #icon>
-				<User />
-			</template>
-			User Settings
-			<template #iconRight>
-				<User />
-			</template>
-		</Button>
-		<Button loading loading-position="left">
-			<template #loadingIcon>
-				<Loader2 class="animate-spin" />
-			</template>
-			Loading...
-		</Button>
-		<Button loading loading-position="right">
-			Loading...
-			<template #loadingIcon>
-				<Loader2 class="animate-spin" />
-			</template>
-		</Button>
-		<Button loading loading-position="center">
-			<template #loadingIcon>
-				<Loader2 class="animate-spin" />
-			</template>
-		</Button>
-		<Button disabled>Disabled</Button>
+		<Tag variant="primary">Hello tag</Tag>
+		<Tag variant="secondary">Hello tag</Tag>
+		<Tag variant="outline">Hello tag</Tag>
+		<Tag variant="ghost">Hello tag</Tag>
+		<Tag variant="danger">Hello tag</Tag>
+		<Tag isActive>Hello tag</Tag>
 	</div>
 </template>
 
